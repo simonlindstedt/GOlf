cdefffdeff5b431d96045fcabc05c6e1ab03a879
simonlindstedt <simon.lindstedt@gmail.com>
2021-04-30 14:08:24 +0200

    new structure

--- src/js/game/Game.js
+++ src/js/game/Game.js
@@ -1,26 +1,45 @@
 import Matter from 'matter-js';
 import Ball from './Ball';
+import * as PIXI from 'pixi.js';
 
 export default class Game {
-  constructor(stageDisplay) {
-    this.stageDisplay = stageDisplay;
-    this.setupWorld();
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    this.app = new PIXI.Application({
+      width: this.width,
+      height: this.height,
+      backgroundAlpha: 0.1,
+    });
+    this.engine = Matter.Engine.create();
+    this.addBodies();
   }
 
-  setupWorld() {
-    this.engine = Matter.Engine.create();
-    // this.engine.gravity = {
-    //   x: 0,
-    //   y: 0,
-    // };
-    this.World = Matter.World;
-    this.ball = new Ball(150, 150, 100);
-    this.ground = Matter.Bodies.rectangle(400, 400, 800, 10, {
-      isStatic: true,
+  addBodies() {
+    this.ball = new Ball(100, 100, 100);
+    this.app.stage.addChild(this.ball.sprite);
+    Matter.World.add(this.engine.world, this.ball.body);
+  }
+
+  setupEvents() {}
+
+  start(debug) {
+    document.body.appendChild(this.app.view);
+    if (debug) {
+      const debugRenderer = Matter.Render.create({
+        engine: this.engine,
+        element: document.body,
+        options: {
+          width: this.width,
+          height: this.height,
+          wireframeBackground: 'transparent',
+        },
+      });
+      Matter.Render.run(debugRenderer);
+    }
+    this.app.ticker.add(() => {
+      this.update();
     });
-    Matter.Body.setAngle(this.ground, 0.5);
-    this.World.add(this.engine.world, [this.ball.body, this.ground]);
-    this.stageDisplay.addChild(this.ball.sprite);
   }
 
   update() {

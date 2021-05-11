c132755a150cd185775da03f293f967004372055
simonlindstedt <simon.lindstedt@gmail.com>
2021-05-11 16:03:32 +0200

    cleanup

--- src/js/game/FlatBall.js
+++ src/js/game/FlatBall.js
@@ -1,8 +1,5 @@
-import { Texture } from '@pixi/core';
 import { Graphics } from '@pixi/graphics';
-import { Sprite } from '@pixi/sprite';
 import { Bodies, Vector, Body, Constraint, Composite } from 'matter-js';
-import ballSprite from 'url:~src/js/game/assets/textures/ball.png';
 import { DropShadowFilter } from 'pixi-filters';
 
 export default class FlatBall {

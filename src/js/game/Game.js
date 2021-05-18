1ec44249cbd58731763d8de39629caeea8eeeafc
simonlindstedt <simon.lindstedt@gmail.com>
2021-05-18 15:45:40 +0200

    restart viewport fix

--- src/js/game/Game.js
+++ src/js/game/Game.js
@@ -164,6 +164,11 @@ export default class Game {
       }
     }
     if (this.winScreen.continue) {
+      this.viewport.scaled = 1;
+      this.viewport.corner = {
+        x: 0,
+        y: 0,
+      };
       this.paused = false;
       this.strikes = 0;
       this.level++;

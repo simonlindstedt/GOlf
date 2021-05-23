e125a1ac086fd003677f52e1f52afbdfb5458860
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-05-23 20:50:42 +0200

    Add winscreen styling

--- src/js/interface/WinScreen.js
+++ src/js/interface/WinScreen.js
@@ -8,17 +8,20 @@ export default class WinScreen {
   render(score) {
     this.active = true;
     this.section = document.createElement('section');
-    this.button = document.createElement('button');
+    this.flexDiv = document.createElement('div');
+    this.flexDiv.classList.add('winscreen-flex-container');
     this.message = document.createElement('h2');
     this.score = document.createElement('p');
+    this.button = document.createElement('button');
 
     this.section.id = 'win-screen';
     this.button.textContent = 'Next Map';
     this.message.textContent = 'You did it!';
     this.score.textContent = `Total strikes: ${score}`;
 
-    this.section.appendChild(this.message);
-    this.section.appendChild(this.score);
+    this.flexDiv.appendChild(this.message);
+    this.flexDiv.appendChild(this.score);
+    this.section.appendChild(this.flexDiv);
     this.section.appendChild(this.button);
 
     const handleClick = () => {
